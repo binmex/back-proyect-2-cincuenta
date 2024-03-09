7bb0bdd17ca6be7576101cea95a4d579b455f51a
DianaDiazDS <93956532+DianaDiazDS@users.noreply.github.com>
2024-03-09 16:21:09 -0500

    afiliado relacion

--- controllers/affileateController.js
+++ controllers/affileateController.js
@@ -1,11 +1,84 @@
 const Afiliado = require("../models/model-affiliate");
 const Results = require("../models/model-Results");
+const Discipline = require("../models/model-discipline");
 
+
+
+
+
+    //  // Agregar reserva al cliente
+    // //  await client.save(); // Guardar el cliente actualizado
+
+
+    //  const newAfiliado = new Afiliado(req.body);
+    //  Discipline.affiliates.push(newAfiliado);
+    // //  await client.save()
+
+
+    // //  afiliado.resultados.push(nuevoResultado);
+    // //  await afiliado.save();
+
+    // //  evento.resultados.push(nuevoResultado);
+    // //  await evento.save();
+
+
+
+
+    // // const newAfiliado = new Afiliado(req.body);
+    // await Discipline.save();
+    // const data = await newAfiliado.save();
+
+
+    createReservation: async (_, args) => {
+      try {
+        const {
+          id,
+          bookingStartDate,
+          bookingEndDate,
+          service,
+          comments,
+          idClient,
+        } = args;
+        const client = await Client.findOne({ id: idClient });
+        if (!client) {
+          throw new Error("Cliente no encontrado");
+        }
+        const reservation = new Reservation({
+          id,
+          bookingStartDate,
+          bookingEndDate,
+          service,
+          comments,
+          client: client._id,
+        });
+        //console.log(reservation);
+        await reservation.save();
+        client.reservations.push(reservation); // Agregar reserva al cliente
+        await client.save(); // Guardar el cliente actualizado
+        return reservation;
+      } catch (error) {
+        throw new Error("Error al crear la reserva: " + error.message);
+      }
+    },
 
 exports.save = async (req, res) => {
   try {
+    
+    const { discipline} = req.body;
+    const idDisci = discipline._id
+    
+    const disciplina = await Discipline.findById( idDisci );
+    if (!disciplina) {
+      return res.status(404).json({ state: false, error: "disciplina no existe" });
+    }   
+
     const newAfiliado = new Afiliado(req.body);
     const data = await newAfiliado.save();
+
+    disciplina.affiliates.push(newAfiliado);
+    await disciplina.save();
+
+    
     res.status(200).json({ state: true, data: data });
   } catch (err) {
     res.status(500).json({ state: false, error: err.message });
@@ -73,13 +146,3 @@ exports.deleteAfiliado = async (req, res) => {
   }
 };
 
-
-// const validateEmail = (correo) => {
-//   var expresionRegularCorreo =
-//     /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-//   return expresionRegularCorreo.test(correo);
-// };
-// const validateCelPhone = (phone) => {
-//   var phoneExpresion = /^[0-9]{10}$/;
-//   return phoneExpresion.test(phone);
-// };
